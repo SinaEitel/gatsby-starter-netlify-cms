bb7e9faa545c0dcf59c43340b2c872314006cee9
Sina <sina.eitel@web.de>
2019-07-04 08:02:59 +0200

    Revert "Update Footer.js"
    This reverts commit f19b7a0597a2cab7cda5479b62b6de65b097263a.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -30,13 +30,18 @@ const Footer = class extends React.Component {
                       </Link>
                     </li>
                     <li>
-                      <Link className="navbar-item" to="/about us">
+                      <Link className="navbar-item" to="/about">
                         About
                       </Link>
                     </li>
                     <li>
                       <Link className="navbar-item" to="/products">
                         Products
+                      </Link>
+                    </li>
+                    <li>
+                      <Link className="navbar-item" to="/contact/examples">
+                        Form Examples
                       </Link>
                     </li>
                     <li>
